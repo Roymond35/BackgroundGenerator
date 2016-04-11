05bcdd1ad82fdcf1e12b4429552b6a405b5845aa
Roy Gero II <roy.gero.ii@gmail.com>
2016-04-11 13:30:05 -0400

    Added the basic layout. Still not completely happy with it but it can work.

--- src/net/roymond/BackgroundGenerator/SetupWindow.java
+++ src/net/roymond/BackgroundGenerator/SetupWindow.java
@@ -1,10 +1,6 @@
 package net.roymond.BackgroundGenerator;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.HierarchyBoundsAdapter;
-import java.awt.event.HierarchyBoundsListener;
-import java.awt.event.HierarchyEvent;
 
 /**
  * Created by Roymond on 4/11/2016.
@@ -13,6 +9,44 @@ public class SetupWindow {
     static JFrame frame;
     private JPanel SetupWindow;
     private JLabel title;
+    private JPanel sizeManager;
+    private JLabel sizeTitle;
+    private JTextField widthField;
+    private JLabel heightText;
+    private JLabel widthText;
+    private JTextField heightField;
+    private JLabel baseColor;
+    private JTextField baseRedValue;
+    private JTextField baseGreenValue;
+    private JTextField baseBlueValue;
+    private JLabel blueTitle;
+    private JLabel greenTitle;
+    private JLabel redTitle;
+    private JPanel baseColorManager;
+    private JLabel shapesTitle;
+    private JCheckBox turnOnCircles;
+    private JCheckBox turnOnSquares;
+    private JCheckBox turnOnPolygons;
+    private JCheckBox turnOnOctagons;
+    private JPanel lockShapesPanel;
+    private JLabel freezeColorsTitle;
+    private JCheckBox freezeRed;
+    private JCheckBox freezeGreen;
+    private JCheckBox freezeBlue;
+    private JTextField deltaValue;
+    private JLabel deltaTitle;
+    private JPanel freezeColorsPanel;
+    private JLabel exportTitle;
+    private JTextField exportDirValue;
+    private JTextField prefixValue;
+    private JLabel exportExtTitle;
+    private JLabel imagePrefix;
+    private JLabel desiredDirTitle;
+    private JTextField exportExt;
+    private JPanel exportManager;
+    private JButton generateButton;
+    private JTextField numberOfRuns;
+    private JLabel numRuns;
 
     public static void main(String[] args) {
         frame = new JFrame("SetupWindow");
