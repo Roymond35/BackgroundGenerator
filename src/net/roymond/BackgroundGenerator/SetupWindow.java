94afc4244e9389ea514bf1058eb5e76601297ce8
Roy Gero <roy.gero.ii@gmail.com>
2016-06-02 11:47:34 -0400

    Adding all of the JFrame variables that, while not being needed in my actual program, are needed to create the window. In other words, Intellij incorrectly flagged these as unused variables and I got caught not testing my changes.
    Bad Roy.

--- src/net/roymond/BackgroundGenerator/SetupWindow.java
+++ src/net/roymond/BackgroundGenerator/SetupWindow.java
@@ -14,12 +14,28 @@ import java.io.File;
 public class SetupWindow {
     static JFrame frame;
     private JPanel SetupWindow;
+    private JLabel title;
+    private JPanel sizeManager;
+    private JLabel sizeTitle;
     private JTextField widthField;
+    private JLabel heightText;
+    private JLabel widthText;
     private JTextField heightField;
+    private JLabel baseColor;
+    private JTextField baseRedValue;
+    private JTextField baseGreenValue;
+    private JTextField baseBlueValue;
+    private JLabel blueTitle;
+    private JLabel greenTitle;
+    private JLabel redTitle;
+    private JPanel baseColorManager;
+    private JLabel shapesTitle;
     private JCheckBox turnOnCircles;
     private JCheckBox turnOnSquares;
     private JCheckBox turnOnPolygons;
     private JCheckBox turnOnOctagons;
+    private JPanel lockShapesPanel;
+    private JLabel freezeColorsTitle;
     private JCheckBox freezeRed;
     private JCheckBox freezeGreen;
     private JCheckBox freezeBlue;
@@ -39,6 +55,17 @@ public class SetupWindow {
     private JLabel blueSliderLabel;
     private JLabel deltaSliderLabel;
     private JButton browse;
+    private JTextField deltaValue;
+    private JLabel deltaTitle;
+    private JPanel freezeColorsPanel;
+    private JLabel exportTitle;
+    private JLabel exportExtTitle;
+    private JLabel imagePrefix;
+    private JLabel desiredDirTitle;
+    private JTextField exportExt;
+    private JPanel exportManager;
+    private JLabel numRuns;
+    private JPanel buttonPanel;
 
     public SetupWindow() {
         errorText.setText(" ");
